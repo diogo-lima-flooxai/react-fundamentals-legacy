49eeafd3047bb20b95c356b6bcb263faec9bb63a
diogo <diogo.lima.flooxai@gmail.com>
2025-05-19 09:55:31 -0300

    feat: add module systems (import / export)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 //import PrimeiroComponente from "./componentes/PrimeiroComponente";
-//import { CompA, CompB as B } from "./componentes/DoisComponentes";
-import MultiElementos from "./componentes/MultiElementos";
+import CompA, { CompB as B } from "./componentes/DoisComponentes";
+// import MultiElementos from "./componentes/MultiElementos";
 
 const elemento = document.getElementById("root");
 const root = createRoot(elemento);
 root.render(
   <div>
-    <MultiElementos />
-    {/* <CompA valor="Olá eu sou A!" />
+    {/* <MultiElementos /> */}
+    <CompA valor="Olá eu sou A!" />
     <B valor="B na área!" /> 
-    <PrimeiroComponente valor='Bom dia'/> */}
+    {/* <PrimeiroComponente valor='Bom dia'/> */}
   </div>
 );
 
-// root.render(<ul>
+// root.render(<ul> 
 //   <l1>1 Pedro</l1>
 //   <l1>2 Maria</l1>
 //   <l1>3 Ana</l1>
